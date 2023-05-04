6be0293cb29dde3f886d14f01dc37c00632b1220
LucaBrivio183 <l.brivio4@gmail.com>
2023-05-04 17:51:40 +0200

    details

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,10 +14,10 @@
                         <div class="card h-100">
                             <div class="card-body">
                               <h5 class="card-title">{{ $movie->title }}</h5>
-                              <h6 class="card-subtitle mb-2 text-body-secondary">{{ $movie->original_title }}</h6>
-                              <p class="card-text">{{ $movie->date }}</p>
-                              <p class="card-text">{{ $movie->nationality }}</p>
-                              <p class="card-text">{{ $movie->vote }}</p>
+                              <h6 class="card-subtitle mb-2 text-body-secondary">Original title: {{ $movie->original_title }}</h6>
+                              <p class="card-text">Date: {{ $movie->date }}</p>
+                              <p class="card-text">Original country: {{ $movie->nationality }}</p>
+                              <p class="card-text">Vote: {{ $movie->vote }}</p>
                             </div>
                           </div>
                     </div>
