f1bfff6064b2b74f92f4986e9a8ee10b7c390322
LucaBrivio183 <l.brivio4@gmail.com>
2023-05-04 16:29:17 +0200

    feat: linked db , added controller and showed data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,26 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>home</title>
          @vite('resources/js/app.js')
     </head>
     <body>
         <div class="container">
-            <h1>Template Classe 89</h1>
-            <img src="{{ Vite::asset('resources/img/pluto.webp') }}" alt="" srcset="">
+            <div class="row">
+                @foreach ($movies as $movie)
+                    <div class="col-3 p-3">
+                        <div class="card h-100">
+                            <div class="card-body">
+                              <h5 class="card-title">{{ $movie->title }}</h5>
+                              <h6 class="card-subtitle mb-2 text-body-secondary">{{ $movie->original_title }}</h6>
+                              <p class="card-text">{{ $movie->date }}</p>
+                              <p class="card-text">{{ $movie->nationality }}</p>
+                              <p class="card-text">{{ $movie->vote }}</p>
+                            </div>
+                          </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
-
     </body>
 </html>
